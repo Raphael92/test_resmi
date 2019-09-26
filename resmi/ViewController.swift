a26be07eef44f1b66f3e0ff937a35f0a60341ef3
Admin <admin@mac-admin.local>
2019-09-26 10:40:09 +0300

    Initial Commit

---
+++ resmi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  resmi
+//
+//  Created by Admin on 26.09.2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
